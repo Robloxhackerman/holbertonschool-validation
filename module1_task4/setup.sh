1c717a6b6a03ff7f9357a0a908b68df16eda0b9b
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-01-07 13:50:27 -0300

    hmmm

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
-sudo apt-get -y install hugo
-sudo apt-get -y install build-essential
-hugo new site dist
+apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
+dpkg -i hugo_0.79.0_Linux-64bit.deb
+make build
