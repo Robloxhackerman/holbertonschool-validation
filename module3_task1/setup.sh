c30c9b7386ee3cfc29ae919d792d0b89fdacecee
Robloxhackerman <zeituniannicolas27@gmail.com>
2023-01-13 12:41:11 -0800

    osofdfdi

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,5 @@
-apt-get update
-apt-get install wget
-apt-get install make
+sudo apt-get update
+apt-get install -y make wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
-exit 0
+make build
