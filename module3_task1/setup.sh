a990d998215f16cb2486017d6d48bb6a6360560f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-01-13 17:53:06 -0300

    ddd

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,5 @@
-apt-get update
-apt-get install wget
-apt-get install make
-wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
-dpkg -i hugo_0.79.0_Linux-64bit.deb
+sudo apt-get update
+sudo apt-get install wget
+sudo apt-get install make
+sudo apt-get install hugo
 exit 0
